ac9a95651e7311f8b64ebd96c6e1c03792d86216
Jeremy Davis <jeremy@turnkeylinux.org>
2015-06-24 13:56:43 +0000

    replace old project with new

--- overlay/var/www/project/views.py
+++
@@ -1,7 +0,0 @@
-
-from django import http
-from django.shortcuts import render_to_response
-
-def index(request, template):
-    http_host = request.META['HTTP_HOST']
-    return render_to_response(template, {'http_host': http_host})
